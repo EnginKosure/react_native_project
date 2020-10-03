4bacd2f0160715018cc0c641c04a04d160bdffb9
EnginKosure <engin.kosure@cronos.be>
2020-10-03 23:45:39 +0200

    config navigation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {View, Text, SafeAreaView} from 'react-native';
 const App = () => {
   return (
-    <SafeAreaView>
-      <View
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{
-          backgroundColor: 'red',
-          margin: 10,
-          padding: 5,
-        }}>
-        <Text>Hello world!</Text>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <View
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            backgroundColor: 'red',
+            margin: 10,
+            padding: 5,
+          }}>
+          <Text>Hello world!</Text>
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 export default App;
