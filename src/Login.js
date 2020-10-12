d518c15a16f1d5302bf419984d1199ee17d81127
EnginKosure <engin.kosure@cronos.be>
2020-10-12 15:00:49 +0200

    add newscard

---
+++ src/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+
+import {Input, Button} from './components';
+
+const Login = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#b2dfdb'}}>
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'android' ? null : 'padding'}>
+        <ScrollView style={{flex: 1}} bounces={false}>
+          <View style={{flex: 1}}>
+            <Image
+              source={require('./assets/cart.png')}
+              style={styles.logoStyle}
+            />
+            <Input holder="Enter e-mail" />
+            <Input holder="Enter password" />
+            <Button text="Enter" />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  logoStyle: {
+    resizeMode: 'contain',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height * 0.4,
+  },
+});
